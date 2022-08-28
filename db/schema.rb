d68331e8c069db8adbafc7b082b67fb375428491
korosdennis <deniptoo98@gmaiil.com>
2022-08-28 18:26:05 +0300

    completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_182218) do
+ActiveRecord::Schema.define(version: 2022_08_28_151815) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
